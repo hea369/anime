be2c6e7d850d260cc00db6862fefe8dee7f538a2
parkhohyun <haor3@hanmail.net>
2022-05-16 23:54:01 +0900

    애니메이션 API연결

--- anime/Main/MainViewController.swift
+++ anime/Main/MainViewController.swift
@@ -7,22 +7,61 @@
 
 import UIKit
 import Nuke
+import Alamofire
+
+struct AnimePosterImage: Codable {
+    let tiny: String
+    let large: String
+    let small: String
+    let medium: String
+    let original: String
+}
+
+struct AnimeAttributes: Codable {
+    let synopsis: String
+    let canonicalTitle: String
+    let posterImage: AnimePosterImage
+}
+
+struct AnimeData: Codable {
+    let id: String
+    let type: String
+    let attributes: AnimeAttributes
+}
+
+struct AnimeModel: Codable {
+    let data: [AnimeData]
+}
 
 class MainViewController: UIViewController {
-    let animeTitles: [String] = ["전생슬", "그랑죠", "원피스", "소아온", "귀칼", "나루토", "나의 히어로 아카데미"]
+    @IBOutlet var tableView: UITableView!
+    var model: AnimeModel?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        AF.request("https://kitsu.io/api/edge/anime?page[limit]=20").response { response in
+            debugPrint(response)
+            guard let data = response.data else { return }
+            let model = try! JSONDecoder().decode(AnimeModel.self, from: data)
+            print(model)
+            self.model = model
+            self.tableView.reloadData()
+        }
+    }
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return animeTitles.count
+        return model?.data.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AnimeTreadingTableViewCell", for: indexPath) as? AnimeTreadingTableViewCell, let url = URL(string: "https://picsum.photos/200/30\(indexPath.row)") else { return UITableViewCell() }
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AnimeTreadingTableViewCell", for: indexPath) as? AnimeTreadingTableViewCell,
+                let data = model?.data[indexPath.row],
+                let url = URL(string: data.attributes.posterImage.medium) else { return UITableViewCell() }
         Nuke.loadImage(with: url, into: cell.animePosterimageView)
-        cell.animeTitleLabel.text = animeTitles[indexPath.row]
-        cell.animeSynobsisLabel.text = "설명입니다설명입니다설명입니다설명입니다설명입니다설명입니다설명입니다설명입니다설명입니다"
+        cell.animeTitleLabel.text = data.attributes.canonicalTitle
+        cell.animeSynobsisLabel.text = data.attributes.synopsis
         return cell
     }
 }
